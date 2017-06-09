de0d22478792e31e01fdaa77b8a0edc30cb209f8
Kamil Cholewiński <harry666t@gmail.com>
2017-06-09 18:15:00 +0200

    Better class, more tests
    Breaks the 2016 API!

--- class.lua
+++ class.lua
@@ -1,27 +1,39 @@
-local class = function(fields)
-    local cls = {}
-    fields = fields or {}
-    cls.extend = function(cls, fields)
-        for k, v in pairs(fields) do
-            cls[k] = v
-        end
-        return cls
+local class
+
+local function inherit(base, fields)
+    assert(base, "inherit must be called with a class as first argument")
+    subcls = class(base)
+    for k, v in pairs(fields) do
+        subcls[k] = v
     end
-    cls:extend(fields)
-    function cls.new(args)
-        local self = {}
-        args = args or {}
-        setmetatable(self, {__index=cls})
-        if self.init then
-            self:init(args)
-        else
-            for k, v in pairs(args) do
-                assert(cls[k])
-                self[k] = v
-            end
+    return subcls
+end
+
+local function new(cls, args)
+    assert(cls, "new must be called with a class as first argument")
+    local self = {}
+    args = args or {}
+    setmetatable(self, {__index=cls})
+    if self.init then
+        self:init(args)
+    else
+        for k, v in pairs(args) do
+            assert(cls[k])
+            self[k] = v
         end
-        self.cls = cls
-        return self
+    end
+    self.cls = cls
+    return self
+end
+
+class = function(fields)
+    local cls = {
+        new     = new,
+        inherit = inherit,
+    }
+    fields = fields or {}
+    for k, v in pairs(fields) do
+        cls[k] = v
     end
     return cls
 end
--- test_class.lua
+++ test_class.lua
@@ -5,25 +5,127 @@ local t = {}
 
 function t:test_instance()
     local Point = class {}
-    local p = Point.new()
+    local p = Point:new()
     assert(p)
 end
 
 function t:test_eq()
     local Point = class {}
-    local p1 = Point.new()
-    local p2 = Point.new()
+    local p1 = Point:new()
+    local p2 = Point:new()
     assert(p1 ~= p2)
 end
 
 function t:test_assign()
     local Point = class {}
-    local p1 = Point.new()
-    local p2 = Point.new()
+    local p1 = Point:new()
+    local p2 = Point:new()
     p1.x = 1
     p2.x = 2
     assert(p1.x == 1)
     assert(p2.x == 2)
+end
+
+function t:test_cls_defaults()
+    local Cat = class {
+        sound = "meow",
+    }
+    function Cat:say() return self.sound end
+    assert(Cat.sound == "meow")
+    assert(Cat:new().sound == "meow")
+    assert(Cat:say() == "meow")
+    assert(Cat:new():say() == "meow")
+    local cat = Cat:new()
+    cat.sound = "mrr"
+    assert(Cat.sound == "meow")
+    assert(Cat:new().sound == "meow")
+    assert(cat:say() == "mrr")
+end
+
+function t:test_cls_inline_function()
+    local Cat = class {
+        sound = "meow",
+        say = function(self) return self.sound end,
+    }
+    assert(Cat.sound == "meow")
+    assert(Cat:new().sound == "meow")
+    assert(Cat:say() == "meow")
+    assert(Cat:new():say() == "meow")
+end
+
+function t:test_inheritance_slurp()
+    local Animal = class {
+        sound = "",
+        say = function(self) return self.sound end,
+    }
+    local Cat = class(Animal)
+    Cat.sound = "meow"
+    assert(Animal.sound == "")
+    assert(Animal:new().sound == "")
+    assert(Animal:say() == "")
+    assert(Animal:new():say() == "")
+    assert(Cat.sound == "meow")
+    assert(Cat:new().sound == "meow")
+    assert(Cat:say() == "meow")
+    assert(Cat:new():say() == "meow")
+end
+
+function t:test_inheritance_inherit()
+    local Animal = class {
+        sound = "",
+        say = function(self) return self.sound end,
+    }
+    local Cat = Animal:inherit {
+        sound = "meow",
+    }
+    assert(Animal.sound == "")
+    assert(Animal:new().sound == "")
+    assert(Animal:say() == "")
+    assert(Animal:new():say() == "")
+    assert(Cat.sound == "meow")
+    assert(Cat:new().sound == "meow")
+    assert(Cat:say() == "meow")
+    assert(Cat:new():say() == "meow")
+end
+
+function t:test_inheritance_custom_new()
+    local Animal = class {
+        sound = "",
+        say = function(self) return self.sound end,
+    }
+    local Cat = Animal:inherit {}
+    Cat.new = function(cls, args)
+        self = {}
+        self.say = function(self) return "meow" end
+        return self
+    end
+    assert(Animal:new():say() == "")
+    assert(Cat:new():say() == "meow")
+end
+
+function t:test_inheritance_custom_inherit()
+    local Animal = class {
+        sound = "",
+        say = function(self) return self.sound end,
+    }
+    local Cat = Animal:inherit {
+        sound = "meow",
+    }
+    Cat.inherit = function(base, fields)
+        subcls = class(base)
+        subcls.base = Cat
+        for k, v in pairs(fields) do
+            subcls[k] = v
+        end
+        return subcls
+    end
+    local Tiger = Cat:inherit {
+        sound = "ROAR",
+    }
+    assert(Animal:new():say() == "")
+    assert(Cat:new():say() == "meow")
+    assert(Tiger:new():say() == "ROAR")
+    assert(Tiger.base == Cat)
 end
 
 return t
